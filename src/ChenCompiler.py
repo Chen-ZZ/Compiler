4834fd3f8f25649a279ba7bad7debd6a1cdcf56a
Tyler Chen Zheng <zhengchenloot@gmail.com>
2015-03-30 11:23:46 +1300

    V0.4 Expression

--- src/ChenCompiler.py
+++ src/ChenCompiler.py
@@ -4,16 +4,16 @@
 # Compiler v0.1 Definition of all tokens.
 # Compiler v0.2 Initialization of symbol table for all tokens.
 # Compiler v0.3 Lexical Analyser(the Scanner), set up print subroutine in the VM.
+# Compiler v0.4 Lexical Analyser(the Scanner), expression processing.
 #===================================================================================
 
-import FileOperator;  
 import sys;
 
-#from FileOperator import *; # Another way to import a py file.
+# from FileOperator import *; # Another way to import a py file.
  
 #===================================================================================
 ### GLOBAL variables Deceleration. ###
-compilerVersion = 0.3;
+compilerVersion = 0.4;
 
 srcFileName = "../code/hello.txt";  # Path to the source code file.
 outputFileName = "../code/hello.obj";  # path for the output machine code obj file.
@@ -23,20 +23,20 @@ linenumber = 0;  # The current line number of source code.
 charIndex = 0;  # The position of current character in the current line.
 EOF = chr(0);  # The end of file character.
 
-outputArray = [];  # An temporary array to hold all machine code/instructions.  
+outputArray = [];  # Temporary array to hold all machine code/instructions.  
+variableInitArray = []  # Temporary array to hold the code for initializing variables
 
 varptr = 900  # Arbitrary base address of memory for variables, which starts from this address.
 codeptr = 10  # Address to output next instruction (for machine code, 1-9 are reserved).
-printStartAddress = 0; # Address to start print subroutine.
+printStartAddress = 0;  # Address to start print subroutine.
 
 token = None;  # A symbol, will be modified by each call of getToken().
-ignoreCharacters = ["\t", " ", "\n"];  # Ignore tabs, spaces and new lines.
 
 # Reserved Token Symbols.
 tokenNames = \
     [ "unknownToken", "codesym", "declare", "assignsym", "idsym", "varsym", "number",
      "leftbrace", "rightbrace", "leftbracket", "rightbracket",
-     "equals", "plus", "minus", "multiply", "devine",
+     "equals", "plus", "minus", "multiply", "divide",
      "semicolon", "comma", "sharp",
      "print", "stringvalue", "endfile" ]
     # More symbol will be added.
@@ -44,7 +44,7 @@ tokenNames = \
 # Define tokens as variables in the compiler.
 unknownToken, codesym, declaresym, assignsym, idsym, varsym, number, \
 leftbrace, rightbrace, leftbracket, rightbracket, \
-equals, plus, minus, multiply, devine, \
+equals, plus, minus, multiply, divide, \
 semicolon, comma, sharp, \
 printsym, stringvalue, endfile = range(0, len(tokenNames));
 
@@ -58,6 +58,7 @@ class symbol:
         self.name = name;
         self.token = token;
         self.value = value;
+        self.address = 0;
 
     def __call__(self):
         self.token.__str__;
@@ -102,7 +103,7 @@ def initSymbolTable():
     addToSymTable('+', plus);
     addToSymTable('-', minus);
     addToSymTable('*', multiply);
-    addToSymTable('/', devine);
+    addToSymTable('/', divide);
     
     addToSymTable(EOF, endfile);
 
@@ -113,7 +114,28 @@ def dumpSymbolTable():
         if tempToken.token == idsym:  # Only print symbols from source code.
             printToken(tempToken);
    
-   
+
+# Open the source code file. 
+def openSourceCode():
+    global srcFileName;
+
+    srcFile = open(srcFileName);
+    src = srcFile.readlines();
+    srcFile.close();
+    
+    return src;
+
+# Save the output to an object file.        
+def saveObjectCode():
+    global outputFileName;
+    global outputArray;
+    
+    f = open(outputFileName, "w")  # open connection to the output file.
+    
+    for s in outputArray:  # for each line in the output array append it to the file.
+        f.write(s + "\n")
+        
+    f.close()  # close the connection to the output file.
             
 # Printer function for given symbol.
 def printToken(inputToken):
@@ -161,6 +183,22 @@ def emit(memoryAddress, operationCode, parameter):
         
     outputArray.append(instruction);        
 
+# Emit the object code for a string.
+def emitString(string):
+    global codeptr;
+    
+    codeptr = codeptr + 1;  # Reserved space for JUMP instruction to ignore string code lines.
+    stringStart = codeptr;  # Remembering the begin of string memory address.
+    
+    for ch in string:  # For each character in the string, put into next memory location.
+        emit(0, "constant", ord(ch));
+        
+    emit(0, "constant", 0);  # Put an 0 after the string memories as the end of it.
+    emit(stringStart - 1, "jump", codeptr);  # Put jump instruction to jump over entire string.
+     
+    emit(0, "loadv", stringStart);  # Put the first character memory address into accumulator.
+    emit(0, "store", 1);  # Put value in accumulator into register.
+
 # Emit a line of comment to the object code. 
 def emitComment(message):   
     global outputArray;
@@ -173,7 +211,7 @@ def emitInit():
     global printStartAddress;
     
     emitComment("Start Of Object Code");
-    codeptr = codeptr + 1; # Researve blank memory address for jump to the actual instructions.
+    codeptr = codeptr + 1;  # Researve blank memory address for jump to the actual instructions.
     
     emitComment("Reserved Print Subroutine");
     
@@ -187,6 +225,17 @@ def emitInit():
     emit(0, "return", None);
     
 #===================================================================================
+# Variable declaration routines.
+def variableInit(memadr, opcode, parameter):
+    global variableInitArray
+    variableInitArray.append([memadr, opcode, parameter])
+
+def emitVraibleInit():
+    global variableInitArray
+    
+    for line in variableInitArray:
+        emit(line[0], line[1], line[2])
+
 
 #===============================================================================
 # Scanner routines
@@ -229,14 +278,14 @@ def getCh():
 def ungetCh():
     global charIndex;
     charIndex = charIndex - 1;
-    
+
 # Get the next valid token.
 def getToken():
     global token;
     global tokenNames;
     
     ch = getCh();  # Get next character.
-    while ch in ignoreCharacters:  # Ignore tabs, spaces and new lines.
+    while ch in ["\t", " ", "\n"]:  # Ignore tabs, spaces and new lines.
         ch = getCh(); 
         
     # If this character is alphabetic then it's a start of a token, either reserved or id.
@@ -264,9 +313,22 @@ def getToken():
         
         ungetCh();  # Jump back to the previous character since it's not used. 
         
-        token = symbol(ch, number, value=intern(tempNumber));  # Set up the number token.
+        token = symbol(ch, number, value=int(tempNumber));  # Set up the number token.
         
         return
+    
+    # Two character tokens.
+    elif ch in [">", "<", "=", "!"]:
+        ch2 = getCh();  # Get next character behind the '='.
+    
+        if ch2 == "=":
+            token = lookup(ch + ch2);
+        else:
+            token = lookup(ch);
+            ungetCh();
+            
+        return
+    
     
     # If this character is a quotation ", then return Token = string & token.value = string_value.
     elif ch == '"':
@@ -288,9 +350,51 @@ def getToken():
         printError("Unknown character detected!");
         sys.exit(1);
 
+#===================================================================================
+# Process variable declaration.
+def variables():
+    global varptr;
+    
+    getToken();  # Skip 'declare' keyword.
+    
+    while(token.token == idsym):  # If this token is an identifier.
+        tempVar = token; 
+        getToken();  # Skip this id token.
+        
+        # print token.name
+        
+        if token.token == assignsym:  # If there is a '=' after the identifier.
+            getToken();  # Skip '=' token.
+            
+            if token.token == number:  # if the token is a number.
+                variableInit(0, "loadv", token.value);
+            else:  # If there is no number after '='.
+                printError("Constant expected to assign a variable.");
+                
+            getToken();  # Skip number token.
+            
+        else:
+            variableInit(0, "loadv", 0);
+        
+        if tempVar.address == 0:  # If var's address in empty means it's a new variable.
+            symtable[tempVar.name].address = varptr;  # Assign memory address to this var.
+            variableInit(0, "store", varptr);  # Store value in accumulator into memory address.
+            varptr = varptr + 1;  # Increment the variable pointer by 1.
+        else:
+            printError("%c already declared before.");
+        
+        if token.token == semicolon:
+            getToken();
+            break;    
+    
+        if token.token == comma:
+            getToken();
+        else:
+            printError("',' expected in variable declaration.");
+
 
 #===================================================================================
-# Main list of statements processing function
+# Process statements.
 def stmtList():
     stmt();  # Process the first statement.
     
@@ -305,25 +409,29 @@ def stmtList():
 def stmt():
     if token.token == printsym:
         printStmt();
+    elif token.token == idsym:
+        assignmentStmt();
+    else: 
+        printError("Start of a statement expected.");
 
 # For each statement, there is a particular statement function.
 def printStmt():
-    emitComment("Start Print Statement"); # Comment to notify start of print statement.
+    emitComment("Start Print Statement");  # Comment to notify start of print statement.
     
-    getToken(); # Skip the "print" key word.
+    getToken();  # Skip the "print" key word.
     
-    if token.token == leftbracket: # "(" expected after "print".
-        getToken(); # Skip "(".
+    if token.token == leftbracket:  # "(" expected after "print".
+        getToken();  # Skip "(".
     else: printError(" '(' expected after 'Print'");
     
-    printItem(); # Process the current item after the bracket.
+    printItem();  # Process the current item after the bracket.
     
     while token.token == comma:
         getToken();
         printItem(); 
         
-    if token.token == rightbracket: # End of print statement.
-        getToken(); # Skip ')'.
+    if token.token == rightbracket:  # End of print statement.
+        getToken();  # Skip ')'.
     else: printError("')' expected as the end of print. ");
     
         
@@ -332,17 +440,107 @@ def printItem():
     global codeptr;
     global printStartAddress;
     
-    # TODO Put a string into memory.
+    if token.token == stringvalue:
+        emitString(token.name);  # Put a string into memory.
+        emit(0, "call", printStartAddress);
+        getToken();  # Skip string token.
+    else:
+        # TODO : expressionStmt();
+        sys.exit();
     
-    getToken(); # Skip string token.
-        
-        
+    emit(0, "loadv", 13);  # Put 13 to accumulator where 13 is a new line character.
+    emit(0, "writech", 0);  # Display the value in accumulator as ascii to the console.
+
+
+def assignmentStmt():
+    emitComment("Start Assginment Statement");  # Comment to notify start of print statement
+    which_ID = token             # Remember which ID on the left side of "="
+    if lookup(which_ID.name) == None: # If this ID has not been declared, then error.
+        printError("Udeclared variable detected.");
+    
+    getToken();  # Skip this ID token.
+    
+    if token.token == assignsym:
+        getToken(); # Skip the '=' token.
+    else:
+        printError("'=' expected in an assignment statement.");
+    
+    expression();
+    
+    
+
 #===================================================================================
+# Process expression.
+# Grammer of expression is expression = term operator term, term = factor operator factor.
+def expression():
+    term();  # Process the first term.
+    
+    while token.token in [plus, minus]:
+        operator = token;  #
+        getToken();  # Skip the '+' or '-' token.
+        
+        # Object code to calculate the left hand side result.
+        
+        term();  # Process the right hand side of the operator.
+        # Object code to calculate the left hand side result, store the result.
+        
+        # # Object code to calculate the final result of both sides of the operator.
+        if operator == plus:
+            return None;
+        elif operator == minus:
+            return None;
+
+# Process a term    
+def term():
+    factor();  # Process the left hand side of the operator.
+    
+    while token.token in [multiply, divide]:
+        operator = token;  # Remember the operator.
+        
+        # Object code to calculate the left hand side result.
+        
+        getToken();  # Skip '*' or '/' token.
+        factor();  # Process the right hand side of the operator, return the result.
+        # Object code to calculate the left hand side result, store the result.
+        
+        # # Object code to calculate the final result of both sides of the operator.
+        if operator == multiply:
+            return None;
+        elif operator == divide:
+            return None;
+        
+# Process a factor
+def factor():
+    if token.token == idsym:  # If the factor is an ID token.
+        idtoken = token;
+        emit(0, "load", idtoken.address);  # Load the value in the ID address into ACC.
+        getToken();  # Skip this number token.       
+    
+    elif token.token == number:  # If the factor is an number token.
+        emit(0, "loadv", token.value);  # Load the value of this number into ACC.
+        getToken();  # Skip this number token.    
+    
+    elif token.token == leftbracket:  # If this expression starts with an '('. 
+        getToken();  # Skip the '('.
+        expression();  # Call expression again.
+        
+        if token.token == rightbracket:  # ')' is expected on the right hand side.
+            getToken();  # Skip the ')'.
+        else: 
+            printError("')' expected as the end of parenthesized expression.");
+    
+    else:
+        printError("Start of factor error detected.");
 
 
 #===================================================================================
 # Main code processing function
 def code():
+    
+    emitInit();  # Emit print subroutine into output array.
+   
+    emit(10, "jump", codeptr)  # Jump over the initial code and function definitions
+    
     getToken();  # Get the first token of the input file.
     # printToken(token);
     
@@ -357,6 +555,11 @@ def code():
     if token.token == semicolon:  # Semicolon is the end of each line.
         getToken();
     else: printError("Semicolen missing...");
+     
+    if token.token == declaresym:  # Process local variables are declaration. 
+        variables();
+    emitComment("Start Declaration")
+    emitVraibleInit()  # Initialize all variables by creating blank memory spaces for them
     
     if token.token == leftbrace:  # "{" is the start of code statements.
         getToken();
@@ -364,6 +567,7 @@ def code():
     
     stmtList();
     
+    emit(0, "halt", None)  # emit halt to stop execution
     
 #===================================================================================
 
@@ -378,19 +582,18 @@ def main():
     global outputFileName;
     global outputArray;
     
-    src = FileOperator.openSourceCode(srcFileName);  # @UndefinedVariable
+    src = openSourceCode();  # @UndefinedVariable
     
     initSymbolTable();
     
-    emitInit(); # Emit print subroutine into output array.
-    
     code();  # Process the input source code.
     
-    DisplayObjectCode() # Display the machine code to a console.
+    DisplayObjectCode();  # Display the machine code to a console.
     
     dumpSymbolTable();
     
-    # saveObjectCode(outputFileName, outputArray) # Save the machine code to a object file.
+    saveObjectCode();  # Save the machine code to a object file.
+    
     sys.exit();
 
 
